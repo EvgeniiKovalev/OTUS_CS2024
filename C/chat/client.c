28b75557be711cf1f0fa86a715f6db22a1f98e97
EvgeniiKovalev <ev-kov@mail.ru>
2024-10-29 07:57:28 +0300

    Доработана обработка ошибок, проверка и очистка памяти, разбитон на несколько функций

--- C/chat/client.c
+++ C/chat/client.c
@@ -1,59 +1,42 @@
-#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <stdbool.h>
-#include <unistd.h>
 
-int connect_to_server(int socket_fd, const struct addrinfo *res, char *port, char *ip) {
-    printf("%s %s:%s%s", "Попытка подключения к ", ip, port, "\n");
-    if (connect(socket_fd, res->ai_addr, res->ai_addrlen) == -1) {
-        perror("connection error");
-        return 1;
-    }
-    printf("Успешно подключились к серверу \n");
-    return 0;
-}
 
-int main() {
+
+int connect_to_server(int socket_fd, const struct addrinfo *res, char *port, char *ip);
+char *get_ip_localhost (int *res);
+
+int main(void) {
     struct addrinfo hints;
     struct addrinfo *res;
     char port[5] = "9002";
-    char hostbuffer[256];
 
     memset(&hints, 0, sizeof hints); // Обнуляем все байты в структуре
+    //char *ip = "255.255.255.255";
 
-    //получаем ip localhost'а
-    if (gethostname(hostbuffer, sizeof(hostbuffer)) == -1) {
-        perror("gethostname");
+    int status_ip = 0;
+    char *ip = get_ip_localhost(&status_ip);
+    //char *ip = "192.168.98.129" //
+    if (status_ip != 0 & ip == NULL) {
+        fprintf(stderr, "Error resolving the ip localhost\n");
         return 1;
     }
-    const struct hostent *he = gethostbyname(hostbuffer);
-    if (he == NULL) {
-        perror("gethostbyname");
-        return 1;
-    }
-    char *ip = inet_ntoa(*((struct in_addr*)he->h_addr_list[0]));
-    //char *ip = "192.168.98.129"; //пример ip отличного от localhost
 
-
-    hints.ai_family = AF_INET;       // ipv4
-    hints.ai_socktype = SOCK_STREAM; // TCP (Stream Sockets)
-
+    printf("%s\n", ip);
     getaddrinfo(ip, port, &hints, &res);
 
-    char ans[64] = "";
+    char ans[7] = "";
     int i = 0;
     while (true) {
         i++;
-        printf(" i = %d\n", i);
+        printf("итерация %d\n", i);
 
-        char msg[64] = "";
+        char msg[7] = "";
 
         int socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
         if (socket_fd == -1) {
@@ -65,17 +48,16 @@ int main() {
             return 1;
         }
         //sleep(10); //проверка таймаута на стороне сервера
-        printf("Введите сообщение для отправки серверу, не более 64 символов: ");
-        scanf("%s", msg);
+        printf("Введите сообщение для отправки серверу, не более 7 символов: ");
+        scanf("%7s", msg);
         int sended = send(socket_fd, msg, strlen(msg), 0);
         if (sended == -1) {
             perror("send");
             return 1;
         }
-        printf("Отправлено байт в сокет %d\n", sended);
+        printf("Отправлено байт в сокет: %d\n", sended);
         //quit - выход для сервера и клиента
-        int cmp = strcmp(msg, "quit");
-        if (cmp !=0) {
+        if (strcmp(msg, "quit") !=0) {
             int received = recv(socket_fd, ans, sizeof(ans), 0);
             if (received == -1) {
                 perror("recv");
@@ -91,6 +73,42 @@ int main() {
         }
     }
 
-    freeaddrinfo(res); // Очищаем результат
+    freeaddrinfo(res);
+    free(ip);
+    return 0;
+}
+
+char *get_ip_localhost(int *res){
+    char hostbuffer[256];
+    //получаем ip localhost'а
+    if (gethostname(hostbuffer, sizeof(hostbuffer)) == -1) {
+        perror("gethostname");
+        *res= 1;
+    }
+    const struct hostent *he = gethostbyname(hostbuffer);
+    if (he == NULL) {
+        perror("gethostbyname");
+        *res= 1;
+        return NULL;
+    }
+    char *ip_address_local = inet_ntoa(*((struct in_addr*)he->h_addr_list[0]));
+    char *ip_address = malloc(sizeof(*ip_address)+1);
+    if (ip_address == NULL) {
+        perror("malloc");
+        *res= 1;
+        return NULL;
+    }
+    memccpy(ip_address, ip_address_local, 0,strlen(ip_address_local));
+    *res= 0;
+    return ip_address;
+}
+
+int connect_to_server(int socket_fd, const struct addrinfo *res, char *port, char *ip) {
+    printf("%s %s:%s%s", "Попытка подключения к ", ip, port, "\n");
+    if (connect(socket_fd, res->ai_addr, res->ai_addrlen) == -1) {
+        perror("connection error");
+        return 1;
+    }
+    printf("Успешно подключились к серверу \n");
     return 0;
 }
--- C/chat/server.c
+++ C/chat/server.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdbool.h>
-#include <time.h>
+#define TIMEOUT_SEC 7
+
+int handle_client(const int connection_fd, int timeout);
 
 int main(int argc, char *argv[]) {
     struct addrinfo hints;
@@ -20,69 +20,82 @@ int main(int argc, char *argv[]) {
     hints.ai_socktype = SOCK_STREAM; // TCP (Stream Sockets)
     hints.ai_flags = AI_PASSIVE;     // Автоматическое определение нашего IP
 
+    struct addrinfo their_addr;
+    socklen_t addr_size = sizeof their_addr;
+
     getaddrinfo(NULL,  port, &hints, &res);
 
     int socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (socket_fd == -1) {
+        perror("socket");
+        return 1;
+    }
+
     if  (bind(socket_fd, res->ai_addr, res->ai_addrlen) == -1) {
         perror("bind");
         return 1;
-    };
+    }
     if (listen(socket_fd, 2) == -1) {
         printf("Не удалось начать прослушивание\n");
         return 1;
     }
-    printf("%s %s%s","Успешно начато прослушивание на порту", port,"\n");;
+    printf("%s %s%s","Успешно начато прослушивание на порту: ", port,"\n");
 
-    struct addrinfo their_addr;
-    socklen_t addr_size = sizeof their_addr;
     int i = 0;
-    char ans[64] = "";
     while (true) {
         i++;
-        printf(" i = %d\n", i);
-        char msg[64] = "";
+        printf("итерация %d\n", i);
         int connection_fd = accept(socket_fd, (struct sockaddr *)&their_addr, &addr_size);
         if (connection_fd == -1) {
             printf("Не удалось открыть сокет для обслуживания входящего соединения с клиентом");
             return 1;
         }
-        printf("Подключился клиент\n");;
-
-        //установка таймаута на соединение подключившегося клиента к серверу
-        int timeout = 7; //секунд
-        time_t seconds = time(NULL);
-        int received = recv(connection_fd, msg, 64, 0);
-        if (received == -1) {
-            printf("Ошибка чтения данных из сокета");
-            return 1;
+        int res_handle_client = handle_client(connection_fd, TIMEOUT_SEC);
+        if (res_handle_client == 3) {
+            continue; //таймаут, итерация для нового подключения
         }
-        if (time(NULL) - seconds > timeout) {
-            close(connection_fd);
-            printf("Таймаут %d", timeout);
-            continue; //ожидаем нового подключения
+        if (res_handle_client == 1) {
+            return res_handle_client;//ошибка
         }
-
-
-        printf("длина сообщения = %lu \n", strlen(msg));
-        printf("получено сообщение от клиента: %s\n", msg);
-        int cmp = strcmp(msg, "quit");
-        if (cmp !=0) {
-            printf("введите сообщение для ответа клиенту, не более 64 символов:");
-            scanf("%s", ans);
-            if (send(connection_fd, ans, strlen(ans), 0) == -1) {
-                perror("send");
-                return 1;
-            }
-
-        }
-        else {
+        if (res_handle_client == 2) {
             printf("Выходим, т.к. получена команда завершения работы сервера и клиента");
             close(connection_fd);
             freeaddrinfo(res); // Очищаем результат
             close(socket_fd);
             break;
         }
+    }
+    return 0;
+}
+
+int handle_client(const int connection_fd, int timeout) {
+    printf("Подключился клиент, установлен таймаут подключения: %d\n", timeout);
+    char msg[7] = "";
+    //установка таймаута на соединение подключившегося клиента к серверу
+    time_t seconds = time(NULL);
+    if (recv(connection_fd, msg, 7, 0) == -1) {
+        printf("Ошибка чтения данных из сокета");
+        return 1;
+    }
+    if (time(NULL) - seconds > timeout) {
         close(connection_fd);
+        printf("Не уложились в таймаут: %d\n", timeout);
+        return 3;
+    }
+
+    printf("длина сообщения = %d \n", strlen(msg));
+    printf("получено сообщение от клиента: %s\n", msg);
+    if (strcmp(msg, "quit") !=0) {
+        char ans[7]= "";
+        printf("введите сообщение для ответа клиенту, не более 7 символов:");
+        scanf("%7s", ans);
+        if (send(connection_fd, ans, strlen(ans), 0) == -1) {
+            perror("send");
+            return 1;
+        }
+    }
+    else {
+        return 2;//завершение по quit
     }
     return 0;
 }
